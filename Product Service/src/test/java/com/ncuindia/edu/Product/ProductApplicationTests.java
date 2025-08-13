77131413164923d8c23b8f19ae7df48ba8e8d38d
Hemant Arora <hemant.arora102004@gmail.com>
2025-08-13 09:00:26 +0530

    Initial Commit

---
+++ Product Service/src/test/java/com/ncuindia/edu/Product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncuindia.edu.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Supplier Service/src/test/java/com/ncuindia/edu/Supplier/SupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncuindia.edu.Supplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
