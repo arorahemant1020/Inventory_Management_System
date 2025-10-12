604515852534257c78efadcd8e746e6bd23d7bd9
Hemant Arora <hemant.arora102004@gmail.com>
2025-10-13 00:42:32 +0530

    Initial Commit

--- configuration/src/main/java/com/ncuindia/edu/configuration/ConfigurationApplication.java
+++ configuration/src/main/java/com/ncuindia/edu/configuration/ConfigurationApplication.java
@@ -7,8 +7,21 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigurationApplication {
-
+	
 	public static void main(String[] args) {
+		// String mode = System.getenv("CONFIG_MODE");
+		// SpringApplication app = new SpringApplication(ConfigurationApplication.class);
+
+		// if("native".equalsIgnoreCase(mode)){
+		// 	app.setAdditionalProfiles("native");
+		// 	System.out.println(mode);
+		// } else{
+		// 	app.setAdditionalProfiles("git");
+		// 	System.out.println(mode);
+		// }
+
+
+		// app.run(args);
 		SpringApplication.run(ConfigurationApplication.class, args);
 	}
 
